1087d8754b9d0ac16b761403f4d558c7704370fe
JohanChan <flugjack47@gmail.com>
2023-11-22 16:52:34 -0600

    Buscar con la ID

--- main.go
+++ main.go
@@ -36,9 +36,27 @@ func postAlbums(c *gin.Context) {
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(200, newAlbum)
 }
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(200, a)
+			return
+		}
+	}
+	c.IndentedJSON(200, gin.H{"message": "album not found"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
