83232dfe246a9111e20241da19cd2d1cf7637347
JohanChan <flugjack47@gmail.com>
2023-11-23 15:21:23 -0600

    Se agrego una base de dato

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
 
 // album represents data about a record album.
 type album struct {
-	ID     string  `json:"id"`
+	ID     string  `json:"ID"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
 
-// albums slice to seed record album data.
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
@@ -54,6 +57,13 @@ func getAlbumByID(c *gin.Context) {
 }
 
 func main() {
+
+	db, err := gorm.Open(sqlite.Open("album.sqlite"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&album{})
+
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
