f7d546cd8194f3cba32ad5e5bc891b1f9d40020c
JohanChan <flugjack47@gmail.com>
2023-11-23 17:24:54 -0600

    Delete and finish

--- main.go
+++ main.go
@@ -8,35 +8,28 @@ import (
 
 // album represents data about a record album.
 type album struct {
-	ID     string  `json:"ID"`
+	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
 // getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(200, albums)
-}
 
 // postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
-	var newAlbum album
+	db, err := gorm.Open(sqlite.Open("album.sqlite"), &gorm.Config{})
 
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
+	if err != nil {
 		return
 	}
 
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+	db.Create(&newAlbum)
 	c.IndentedJSON(200, newAlbum)
 }
 
@@ -44,16 +37,49 @@ func postAlbums(c *gin.Context) {
 // parameter sent by the client, then returns that album as a response.
 func getAlbumByID(c *gin.Context) {
 	id := c.Param("id")
+	db, err := gorm.Open(sqlite.Open("album.sqlite"), &gorm.Config{})
 
-	// Loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(200, a)
-			return
-		}
+	if err != nil {
+		return
 	}
-	c.IndentedJSON(200, gin.H{"message": "album not found"})
+
+	var album album
+	err = db.First(&album, id).Error
+
+	if err != nil {
+		return
+	}
+
+	c.IndentedJSON(200, &album)
+}
+func getAlbums(c *gin.Context) {
+	db, err := gorm.Open(sqlite.Open("album.sqlite"), &gorm.Config{})
+	if err != nil {
+		return
+	}
+
+	var albums []album
+	result := db.Find(&albums)
+
+	if result.Error != nil {
+		return
+	}
+	c.IndentedJSON(200, albums)
+
+}
+
+func deletAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+	db, err := gorm.Open(sqlite.Open("album.sqlite"), &gorm.Config{})
+
+	if err != nil {
+		return
+	}
+
+	err = db.Delete(&album{}, id).Error
+
+	c.IndentedJSON(200, gin.H{"mensaje": "Elementos borrados correctamente"})
+
 }
 
 func main() {
@@ -65,8 +91,9 @@ func main() {
 	db.AutoMigrate(&album{})
 
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
+	router.DELETE("/albums/:id", deletAlbumByID)
+	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
